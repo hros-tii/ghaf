defcae2b87418c5c3d1a97afe0537fef9e3585db
Hugo Ros <Hugo.Ros@tii.ae>
2026-08-20 13:56:35 +0300

    feat(crosvm): Add ghaf.virtualization.crosvm option to customize crosvm attributes within Ghaf
    Signed-off-by: Hugo Ros <Hugo.Ros@tii.ae>

---
+++ modules/common/virtualization/crosvm.nix
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: 2022-2026 TII (SSRC) and the Ghaf contributors
+# SPDX-License-Identifier: Apache-2.0
+#
+# Module to customize the crosvm package used by microvm and its compilation
+# options.
+#
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
+  _file = ./crosvm.nix;
+
+  options.ghaf.virtualization.crosvm = {
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkgs.crosvm;
+      defaultText = lib.literalExpression "pkgs.crosvm";
+      description = "The crosvm package used across Ghaf modules.";
+    };
+
+    features = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "Additional features to compile in crosvm binary";
+    };
+
+    patches = lib.mkOption {
+      type = lib.types.listOf lib.types.path;
+      default = [ ];
+      description = "Patches to apply to crosvm.";
+    };
+
+    logLevel = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = "debug";
+      example = "info";
+      description = ''
+        Value for crosvm `--log-level` option, applied to every microVM
+        launched with the crosvm hypervisor. Set to null to leave at built-in
+        default.
+      '';
+    };
+  };
+}
